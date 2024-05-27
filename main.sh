7939058adecbe6fa402f3be04d9971bd2c9be10a
liyq <liyq@joyargroup.com>
2024-05-27 11:45:52 +0800

    增加hy2协议一键脚本

---
+++ main.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
+fi
+
+installHysteria2(){
+    wget https://raw.githubusercontent.com/benjamen888/vps-vpn/proxytype/hy2.sh && bash hy2.sh	
+}
+
+unInstallHysteria2(){
+    bash <(curl -fsSL https://get.hy2.sh/) --remove
+	rm -rf /etc/hysteria
+}
+runmenu(){
+    clear
+    echo " ================================================== "
+    echo " 介绍：一键安装 Reality和hy2代理    "
+    echo " 系统：Ubuntu、Debian、CentOS                        "
+    echo " ================================================== "
+    echo " 1. 安装 Reality"
+    echo " 2. 安装 Hysteria2"
+	echo " ------------------------------------"
+	echo " 12.卸载Hysteria2"
+    echo " 0. 退出脚本"
+    echo
+    read -p "请输入数字:" num
+    case "$num" in
+    1)
+    InstallReality
+    2)
+    InstallHysteria2
+    ;;
+    12)
+    unInstallHysteria2
+    ;;	
+    0)
+    exit 1
+    ;;
+    *)
+    clear
+    echo "请输入正确数字"
+    sleep 2s
+    runmenu
+    ;;
+    esac
+}
+
+timedatectl set-timezone Asia/Shanghai
+
+runmenu
---
+++ proxytype/hy2.sh
@@ -0,0 +1,121 @@
+#!/bin/sh
+
+if [[ $EUID -ne 0 ]]; then
+    clear
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
+fi
+
+
+hyPasswd=$(cat /proc/sys/kernel/random/uuid)
+getPort=12345
+# Function to check if a port is available
+check_port() {
+  local port=$1
+  if (echo > /dev/tcp/127.0.0.1/$port) &>/dev/null; then
+    return 1  # Port is in use
+  else
+    return 0  # Port is available
+  fi
+}
+
+# Generate a random port and check its availability
+while true; do
+  getPort=$(shuf -i 10000-20000 -n 1)
+  if check_port $port; then
+    echo "Available port: $port"
+    break
+  else
+    echo "Port $port is in use, trying another..."
+  fi
+done
+
+
+
+getIP(){
+    local serverIP=
+    serverIP=$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
+    if [[ -z "${serverIP}" ]]; then
+        serverIP=$(curl -s -6 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
+    fi
+    echo "${serverIP}"
+}
+
+install_hy2(){
+    if [ -f "/usr/bin/apt-get" ]; then
+        apt-get update -y && apt-get upgrade -y
+        apt-get install -y gawk curl
+    else
+        yum update -y && yum upgrade -y
+        yum install -y epel-release
+        yum install -y gawk curl
+    fi
+    bash <(curl -fsSL https://get.hy2.sh/)
+    mkdir -p /etc/hysteria/
+    openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500 && chown hysteria /etc/hysteria/server.key && chown hysteria /etc/hysteria/server.crt
+
+cat >/etc/hysteria/config.yaml <<EOF
+listen: :$getPort
+tls:
+  cert: /etc/hysteria/server.crt
+  key: /etc/hysteria/server.key
+
+auth:
+  type: password
+  password: $hyPasswd
+  
+masquerade:
+  type: proxy
+  proxy:
+    url: https://bing.com
+    rewriteHost: true
+quic:
+  initStreamReceiveWindow: 26843545 
+  maxStreamReceiveWindow: 26843545 
+  initConnReceiveWindow: 67108864 
+  maxConnReceiveWindow: 67108864 
+EOF
+
+systemctl enable hysteria-server.service && systemctl restart hysteria-server.service && systemctl status --no-pager hysteria-server.service
+rm -f main.sh hy2.sh
+
+cat >/etc/hysteria/hyclient.json<<EOF
+{
+===========配置参数=============
+代理模式：Hysteria2
+地址：$(getIP)
+端口：${getPort}
+密码：${hyPasswd}
+SNI：bing.com
+传输协议：tls
+跳过证书验证：ture
+====================================
+hysteria2://$(echo -n "${hyPasswd}@$(getIP):${getPort}/?insecure=1&sni=bing.com#hy2-9527")
+}
+EOF
+
+    clear
+}
+
+client_hy2(){
+    hylink=$(echo -n "${hyPasswd}@$(getIP):${getPort}/?insecure=1&sni=bing.com#hy2-9527")
+
+    echo
+    echo "安装已经完成"
+    echo
+    echo "===========Hysteria2配置参数============"
+    echo
+    echo "地址：$(getIP)"
+    echo "端口：${getPort}"
+    echo "密码：${hyPasswd}"
+    echo "SNI：bing.com"
+    echo "传输协议：tls"
+    echo "打开跳过证书验证，true"
+    echo
+    echo "========================================="
+    echo "hysteria2://${hylink}"
+    echo
+}
+
+install_hy2
+client_hy2
